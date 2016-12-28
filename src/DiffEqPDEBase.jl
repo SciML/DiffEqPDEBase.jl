902053e46bf11fd6e08bcc6b1673ae5b90a59c30
ChrisRackauckas <me@chrisrackauckas.com>
2016-12-27 20:26:54 -0800

    algorithm dispatch

--- src/DiffEqPDEBase.jl
+++ src/DiffEqPDEBase.jl
@@ -6,14 +6,23 @@ using DiffEqBase
 
 using RecipesBase, VectorizedRoutines.Matlab, ChunkedArrays
 
-abstract AbstractPoissonProblem <: DEProblem
-abstract AbstractHeatProblem <: DEProblem
+abstract PDEProblem <: DEProblem
+abstract AbstractPoissonProblem <: PDEProblem
+abstract AbstractHeatProblem <: PDEProblem
 
 abstract AbstractFEMSolution <: DESolution
 
 "`Mesh`: An abstract type which holds a (node,elem) pair and other information for a mesh"
 abstract AbstractMesh
 abstract AbstractFEMMesh <: AbstractMesh
+
+abstract PDEAlgorithm <: DEAlgorithm
+abstract AbstractFEMAlgorithm <: PDEAlgorithm
+abstract AbstractFDMAlgorithm <: PDEAlgorithm
+
+# Specific Problem Algorithms
+abstract AbstractHeatFEMAlgorithm <: AbstractFEMAlgorithm
+abstract AbstractPoissonFEMAlgorithm <: AbstractFEMAlgorithm
 
 import Base: size, length, start, next, done, eltype
 
@@ -29,7 +38,11 @@ include("mesh_tools/fem_boundary.jl")
 # Types
 export HeatProblem, PoissonProblem, FEMSolution, FEMMesh, SimpleFEMMesh, FDMMesh
 
-export AbstractPoissonProblem, AbstractHeatProblem, AbstractFEMSolution
+export AbstractPoissonProblem, AbstractHeatProblem, AbstractFEMSolution, PDEProblem
+
+export PDEAlgorithm, AbstractFEMAlgorithm, AbstractFDMAlgorithm
+
+export AbstractHeatFEMAlgorithm, AbstractPoissonFEMAlgorithm
 
 #FEM Functions
 export  assemblematrix, findboundary, setboundary, findbdtype, getL2error, quadpts, getH1error,
