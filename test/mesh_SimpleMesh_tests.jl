7dd7a4e57f51fbc5c4168c6db46ce0a351b24a74
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-24 03:02:24 -0800

    deprecate

--- test/mesh_SimpleMesh_tests.jl
+++
@@ -1,6 +0,0 @@
-using DiffEqPDEBase
-
-pdeprob = prob_poisson_wave
-mesh = SimpleFEMMesh(pdeprob.mesh.node,pdeprob.mesh.elem)
-
-true
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,2 @@
-using DiffEqPDEBase, FiniteElementDiffEq, DiffEqProblemLibrary
+using DiffEqPDEBase
 using Base.Test
-
-const FILEIO_ENABLE = false
-
-println("Quadrature Points Tests")
-@time @test include("quadpts_test.jl")
-println("Boundary Tests")
-FILEIO_ENABLE && @time @test include("boundary_tests.jl")
-println("Example Mesh Tests")
-FILEIO_ENABLE && @time @test include("mesh_examples_tests.jl")
-println("Simple Mesh Tests")
-@time @test include("mesh_SimpleMesh_tests.jl")
